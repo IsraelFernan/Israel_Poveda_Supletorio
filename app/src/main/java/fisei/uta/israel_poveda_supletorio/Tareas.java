c58e4a63743c8b218cfbbc04d6273095d680006c
IsraelFernan <alexlopescado@gmail.com>
2022-08-01 17:04:12 -0500

    InterfazPersonal

--- app/src/main/java/fisei/uta/israel_poveda_supletorio/Tareas.java
+++ app/src/main/java/fisei/uta/israel_poveda_supletorio/Tareas.java
@@ -14,7 +14,7 @@ public class Tareas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tareas);
         ImageView IPeliminar = findViewById(R.id.IPeliminar);
-        Picasso.get().load()
+        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaR-j-nYEgA-B7qQD3z0hCM8eUwNSfH-3_Fw&usqp=CAU").into(IPeliminar);
         ImageView IPiconoLista = findViewById(R.id.IPiconoLista);
         Picasso.get().load("https://cdn.pixabay.com/photo/2017/06/10/07/18/list-2389219_960_720.png").into(IPiconoLista);
     }
