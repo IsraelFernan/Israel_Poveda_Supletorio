1b0e83dda03b4a4b337f41f6631a3fe84ab2781d
IsraelFernan <alexlopescado@gmail.com>
2022-08-01 16:47:39 -0500

    InterfazTareas

--- app/src/main/java/fisei/uta/israel_poveda_supletorio/Tareas.java
+++ app/src/main/java/fisei/uta/israel_poveda_supletorio/Tareas.java
@@ -3,6 +3,9 @@ package fisei.uta.israel_poveda_supletorio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class Tareas extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class Tareas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tareas);
+        ImageView IPeliminar = findViewById(R.id.IPeliminar);
+        Picasso.get().load()
+        ImageView IPiconoLista = findViewById(R.id.IPiconoLista);
+        Picasso.get().load("https://cdn.pixabay.com/photo/2017/06/10/07/18/list-2389219_960_720.png").into(IPiconoLista);
     }
 }
