e72548ddb8ac1d7b05df7257b7c0136ba6c81a70
IsraelFernan <alexlopescado@gmail.com>
2022-08-01 19:43:42 -0500

    ResolviendoErrores

--- app/src/main/java/fisei/uta/israel_poveda_supletorio/Tipotarea.java
+++ app/src/main/java/fisei/uta/israel_poveda_supletorio/Tipotarea.java
@@ -2,7 +2,10 @@ package fisei.uta.israel_poveda_supletorio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Tipotarea extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Tipotarea extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipotarea);
+        Button btnTipoPersonal = findViewById(R.id.btnTipoPersonal);
+        Button btnTipoNegocios = findViewById(R.id.btnTipoNegocios);
+        btnTipoNegocios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Tipotarea.this,Agregartarea.class);
+                i.putExtra("tipo","Negocios");
+                startActivity(i);
+            }
+        });
+        btnTipoPersonal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Tipotarea.this,Agregartarea.class);
+                i.putExtra("tipo","Personal");
+                startActivity(i);
+            }
+        });
     }
 }
